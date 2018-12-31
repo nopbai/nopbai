8bf79220eaa0576771806a7d854dc71dc6395184
unknown <maithuan147>
2018-12-31 16:25:26 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title></title>
+    </head>
+    <body>
+    <div class="main">
+    <div class="chia">
+    	<p class="off"><i>0effortthemes!</i></p>
+    	<p class="so">603 X 901</p>
+    </div>
+    <div class="tomau">
+    <div class="to">
+     <h1 class="col">Sign Up</h1>
+     <p class="chu">Please fill your infomation bellow</p>
+     <form class="th">
+     	<p class="colt">FULL NAME<input  type="text" name="FULL NAME" placeholder ="Jack Dowsun"></p>
+     	<p class="colt">EMAIL ID<input class="cole" type="text" name="EMAIL ID" placeholder ="Jack@demoid.com">
+     	<p class="colt">PHONE NO<input class="colh" type="text" name="PHONE NO" placeholder ="+91"></p>
+     	<p class="colt">PASSWORD<input class="colh" type="text" name="PASSWORD" placeholder ="* * * *"></p>
+     	<p class="colt">WEBSITE<input class="colh" type="text" name="WEBSITE" ></p>
+
+     </form>
+     <p><input type="checkbox" name="checkbox"  >I Accept <a href="#" class="tr">terms and conditions </a>  & privacy policy</p>
+
+     <button class="bt">LOGIN</button>
+
+     <p>Login with social media</p>
+     <nav class="h">
+     	<a href="#"><img src="img/iconfb.jpg"></a>
+     	<a href="#"><img src="img/iconlinkedin.jpg"></a>
+     	<a href="#"><img src="img/icontwitter.jpg"></a>
+     	<a href="#"><img src="img/iconyoutube.jpg"></a>
+
+     </nav>
+     </div>
+     </div>
+     </div>
+    </body>
+</html>
